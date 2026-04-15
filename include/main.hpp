0caa0daba89b4d09b898e6e141156d038ef99951
darudecookie <darudecookie@gmail.com>
2026-04-15 14:29:11 -0700

    updated main logic and consts

--- include/main.hpp
+++ include/main.hpp
@@ -5,46 +5,61 @@
 
 #include <Arduino.h>
 #include <Adafruit_BNO055.h>
+#include <imumaths.h>
 
 #include "SysStatemachine/SysStatemachine.hpp"
+//#include "CommunicationHandler/CommunicationHandler.hpp"
+
 #include "GNCMath/Controllers/BDotController/BDotController.hpp"
 #include "GNCMath/Controllers/OmegaSlewController/OmegaSlewController.hpp"
 #include "GNCMath/Controllers/QuaternionPointingController/QuaternionPointingController.hpp"
-#include "GNCMath/ActuatorHandler/ActuatorHandler.hpp"
+
 #include "MTController/MTController.hpp"
+#include "Sensor/AnalogSensor/TempSensor/TempSensor.hpp"
+#include "Sensor/AnalogSensor/CurrentSensor/CurrentSensor.hpp"
 
 namespace io_params
 {
 
-    static const char WiFi_network_name[] = "HSL_magnetorquer_demo";
-    static const char WiFi_password[] = "fTF2Z#(;,xRD'sr4hB`%kG";
-    static constexpr std::array<uint32_t, 3> PC_IP_ADDRESS[] = {10, 19, 111, 237};
-    static constexpr uint32_t PC_PORT_NUM = 10000;
+    static const char WIFI_NETWORK_NAME[] = "HSL_magnetorquer_demo";
+    static const char WIFI_NETWORK_PASSWORD[] = "fTF2Z#(;,xRD'sr4hB`%kG";
+    static const uint8_t WIFI_SERVER_MAX_CLIENTS = 2;
+    static constexpr std::array<uint8_t, 4> WIFE_SERVER_IP_ADDR = {10, 19, 111, 237};
+    static constexpr uint32_t WIFI_SERVER_PORT_NUM = 10000;
 
     // MUST BE PWM PINS
-    static constexpr std::array<std::array<uint8_t, 2>, 3> MT_CONTROL_PINS = {std::array<uint8_t, 2>{0, 1},
-                                                                              std::array<uint8_t, 2>{0, 1},
-                                                                              std::array<uint8_t, 2>{0, 1}};
+    static constexpr std::array<std::array<uint8_t, 2>, NUM_MAGNETORQUERS> MT_CONTROL_PINS = {std::array<uint8_t, 2>{2, 4},
+                                                                                              std::array<uint8_t, 2>{16, 17},
+                                                                                              std::array<uint8_t, 2>{5, 18}};
+
+    // MUST BE ANALOG PINS
+    static constexpr double TEMP_SENS_VOLTS_PER_DEG = .010; // https://www.analog.com/media/en/technical-documentation/data-sheets/TMP35_36_37.pdf: 10mV / *C
+    static constexpr std::array<uint8_t, NUM_MAGNETORQUERS> TEMP_SENSOR_PINS = {13, 12, 14};
+
+    static constexpr double CURRENT_SENS_VOLTS_PER_AMP = 1; // https://www.adafruit.com/product/1164: 1V / A
+    static constexpr std::array<uint8_t, NUM_MAGNETORQUERS> CURRENT_SENSOR_PINS = {27, 26, 25};
 
 } // namespace io_params
 
 namespace sys_params
 {
-    static constexpr std::array<float, 3> MT_CURRENT_LIM_A = {0.182, 0.182, 0.182},
-                                          MT_VOLTAGE_LIM_V = {1e-5, 1e-5, 1e-5};
-    static constexpr float SUPPLY_VOLTAGE_V = 1;
+    static constexpr std::array<float, NUM_MAGNETORQUERS> MT_CURRENT_LIM_A = {0.182, 0.182, 0.182},
+                                                          MT_VOLTAGE_LIM_V = {12, 12, 12};
+    static constexpr float SUPPLY_VOLTAGE_V = 9;
 
-    static constexpr double B_DOT_CONN_kP = 1,
-                            SLEW_CONN_kP = 1,
+    static constexpr double B_DOT_CONN_kP = 1;
+
+    static constexpr double SLEW_CONN_kP = 1,
                             SLEW_CONN_kI = 0,
-                            SLEW_CONN_kD = 0,
-                            QUAT_POINT_CONN_kP = 1,
-                            QUAT_POINT_CONN_kD = 1;
+                            SLEW_CONN_kD = 0;
+
+    static constexpr double QUAT_POINT_CONN_kP = 1,
+                            QUAT_POINT_CONN_kD = 0;
 
     static constexpr float MT_CURRENT_WATCHDOG_CHECK_FREQ_HZ = 200,
                            IMU_POLL_FREQ_HZ = 100,
                            MT_CONTROL_FREQ_HZ = 5,
-                           COMMUNICATION_WRITE_FREQ_HZ = 200,
+                           COMMUNICATION_WRITE_FREQ_HZ = 10,
                            DEBUG_LED_FREQ_HZ = 10;
 
 } // namespace sys_params
@@ -53,19 +68,17 @@ namespace curr_att
 {
 
     static imu::Quaternion curr_attitude = {1, 0, 0, 0};
-    static imu::Vector<3> curr_ang_vel = {0, 0, 0},
-                          curr_ang_acc = {0, 0, 0},
-                          curr_B = {0, 0, 0},
-                          curr_B_dot = {0, 0, 0};
+    static imu::Vector<NUM_MAGNETORQUERS> curr_ang_vel = {0, 0, 0},
+                                          curr_ang_acc = {0, 0, 0},
+                                          curr_B = {0, 0, 0},
+                                          curr_B_dot = {0, 0, 0};
 
 } // namespace curr_att
 
 namespace loop_objs
 {
-    static imu::Quaternion target_attitude = {1, 0, 0, 0};
-    static imu::Vector<3> target_ang_vel = {0, 0, 0};
-
     static sys_st::SysStateMachine sys_controller(sys_st::possible_st::deactivated);
+    //static comm_handle::TargetUpdate system_target;
 
     static gnc_math::conn::BDotController b_dot_conn(sys_params::B_DOT_CONN_kP);
     static gnc_math::conn::OmegaSlewController omega_slew_conn(sys_params::SLEW_CONN_kP, sys_params::SLEW_CONN_kI, sys_params::SLEW_CONN_kD);
@@ -73,31 +86,53 @@ namespace loop_objs
 
     static uint32_t last_MT_watchdog_check_time_ms = 0,
                     last_IMU_check_time_ms = 0,
-                    last_MT_control_check_time_ms = 0;
+                    last_MT_control_check_time_ms = 0,
+                    last_comm_write_check_time_ms = 0,
+                    last_LED_check_time_ms = 0;
 } // namespace loop_objs
 
 namespace io_objs
 {
+   // static comm_handle::CommunicationHandler comm_handler(io_params::WIFE_SERVER_IP_ADDR, io_params::WIFI_SERVER_PORT_NUM, io_params::WIFI_SERVER_MAX_CLIENTS);
+
     static Adafruit_BNO055 BNO055_IMU;
 
-    static std::array<mt_conn::MTController, 3> magnetorquers = {mt_conn::MTController(io_params::MT_CONTROL_PINS[0][0],
-                                                                                       io_params::MT_CONTROL_PINS[0][1],
-                                                                                       sys_params::MT_VOLTAGE_LIM_V[0] / sys_params::SUPPLY_VOLTAGE_V,
-                                                                                       loop_objs::sys_controller),
-                                                                 mt_conn::MTController(io_params::MT_CONTROL_PINS[1][0],
-                                                                                       io_params::MT_CONTROL_PINS[1][1],
-                                                                                       sys_params::MT_VOLTAGE_LIM_V[0] / sys_params::SUPPLY_VOLTAGE_V,
-                                                                                       loop_objs::sys_controller),
-                                                                 mt_conn::MTController(io_params::MT_CONTROL_PINS[2][0],
-                                                                                       io_params::MT_CONTROL_PINS[2][1],
-                                                                                       sys_params::MT_VOLTAGE_LIM_V[0] / sys_params::SUPPLY_VOLTAGE_V,
-                                                                                       loop_objs::sys_controller)};
+    static std::array<mt_conn::MTController, NUM_MAGNETORQUERS> magnetorquers = {mt_conn::MTController(io_params::MT_CONTROL_PINS[0][0],
+                                                                                                       io_params::MT_CONTROL_PINS[0][1],
+                                                                                                       sys_params::MT_VOLTAGE_LIM_V[0] / sys_params::SUPPLY_VOLTAGE_V,
+                                                                                                       loop_objs::sys_controller),
+                                                                                 mt_conn::MTController(io_params::MT_CONTROL_PINS[1][0],
+                                                                                                       io_params::MT_CONTROL_PINS[1][1],
+                                                                                                       sys_params::MT_VOLTAGE_LIM_V[0] / sys_params::SUPPLY_VOLTAGE_V,
+                                                                                                       loop_objs::sys_controller),
+                                                                                 mt_conn::MTController(io_params::MT_CONTROL_PINS[2][0],
+                                                                                                       io_params::MT_CONTROL_PINS[2][1],
+                                                                                                       sys_params::MT_VOLTAGE_LIM_V[0] / sys_params::SUPPLY_VOLTAGE_V,
+                                                                                                       loop_objs::sys_controller)};
+
+    static std::array<sensor::TempSensor, NUM_MAGNETORQUERS> temp_sensors = {sensor::TempSensor(io_params::TEMP_SENSOR_PINS[0], io_params::TEMP_SENS_VOLTS_PER_DEG),
+                                                                             sensor::TempSensor(io_params::TEMP_SENSOR_PINS[1], io_params::TEMP_SENS_VOLTS_PER_DEG),
+                                                                             sensor::TempSensor(io_params::TEMP_SENSOR_PINS[2], io_params::TEMP_SENS_VOLTS_PER_DEG)};
+
+    static std::array<sensor::CurrentSensor, NUM_MAGNETORQUERS> current_sensors = {sensor::CurrentSensor(io_params::CURRENT_SENSOR_PINS[0], io_params::CURRENT_SENS_VOLTS_PER_AMP),
+                                                                                   sensor::CurrentSensor(io_params::CURRENT_SENSOR_PINS[1], io_params::CURRENT_SENS_VOLTS_PER_AMP),
+                                                                                   sensor::CurrentSensor(io_params::CURRENT_SENSOR_PINS[2], io_params::CURRENT_SENS_VOLTS_PER_AMP)};
 
 } // namespace sensor_obs
+
+namespace task_funcs
+{
+    void check_watchdogs() noexcept;
+    void update_IMU_data(const uint32_t dt_ms) noexcept;
+    void control_magnetorquers() noexcept;
+    void write_comm(const uint32_t current_time_ms) noexcept;
+    void update_LEDs() noexcept;
+} // namespace task_funcs
 
 namespace util_funcs
 {
     imu::Vector<3> vector_deriv(const imu::Vector<3> &curr,
                                 const imu::Vector<3> &prev,
-                                const uint32_t dt_ms);
+                                const uint32_t dt_ms) noexcept;
+
 } // namespace util_funcs
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,34 @@
+/*
+
+this code is a frankenstein of old rw and less old adcs stuff shamelessly copy-pasted
+there is a lot of global state but its in namespaces so dont @ me
+if something looks weird i probably wrote it drunk
+
+*/
+
 #include "main.hpp"
+
+#include "GNCMath/ActuatorHandler/ActuatorHandler.hpp"
 
 void setup()
 {
-    io_objs::BNO055_IMU.begin();
+    //    io_objs::comm_handler.report_error(io_objs::comm_handler.init_wifi(io_params::WIFI_NETWORK_NAME, io_params::WIFI_NETWORK_PASSWORD), millis());
+
+    // while (true)
+    {
+        //      io_objs::comm_handler.report_error(io_objs::comm_handler.attempt_client_conn(), millis());
+        delay(500);
+    }
+    Serial.begin(115200);
+    if (io_objs::BNO055_IMU.begin() == false)
+    {
+        Serial.println("meow");
+        // io_objs::comm_handler.report_error(comm_handle::error_codes::imu_init_fail, millis());
+    }
+    while (1)
+    {
+        Serial.println(io_objs::BNO055_IMU.getQuat().w());
+    }
 }
 
 void loop()
@@ -12,62 +38,113 @@ void loop()
     // i dont want to deal with an rtos so were doing it the old fashioned way
     if ((curr_time_ms - loop_objs::last_MT_watchdog_check_time_ms) > static_cast<uint32_t>(1e3 / sys_params::MT_CURRENT_WATCHDOG_CHECK_FREQ_HZ))
     {
-
+        task_funcs::check_watchdogs();
         loop_objs::last_MT_watchdog_check_time_ms = curr_time_ms;
     }
 
-    const auto dt_ms = curr_time_ms - loop_objs::last_IMU_check_time_ms;
+    const auto dt_ms = curr_time_ms - loop_objs::last_IMU_check_time_ms; // we use this a couple of times so gonna define it here
     if (dt_ms > static_cast<uint32_t>(1e3 / sys_params::IMU_POLL_FREQ_HZ))
     {
-        curr_att::curr_attitude = io_objs::BNO055_IMU.getQuat();
-
-        const auto new_ang_vel = io_objs::BNO055_IMU.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-        curr_att::curr_ang_acc = util_funcs::vector_deriv(new_ang_vel, curr_att::curr_ang_vel, dt_ms);
-        curr_att::curr_ang_vel = std::move(new_ang_vel);
-
-        const auto new_B = io_objs::BNO055_IMU.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
-        curr_att::curr_B_dot = util_funcs::vector_deriv(new_B, curr_att::curr_B, dt_ms);
-        curr_att::curr_B = std::move(new_B);
-
+        task_funcs::update_IMU_data(dt_ms);
         loop_objs::last_IMU_check_time_ms = curr_time_ms;
     }
 
     if ((curr_time_ms - loop_objs::last_MT_control_check_time_ms) > static_cast<uint32_t>(1e3 / sys_params::MT_CONTROL_FREQ_HZ))
     {
-        const auto curr_st = loop_objs::sys_controller.get_state();
-        if (curr_st == loop_objs::sys_controller.get_state())
-        {
-            for (int i = 0; i < 3; i++)
-            {
-                io_objs::magnetorquers[i].drive(0);
-            }
-        }
-        else
-        {
-            imu::Vector<3> desired_torque(0, 0, 0);
-            switch (loop_objs::sys_controller.get_state())
-            {
-            case sys_st::possible_st::direct_B_control:
-                break;
-            case sys_st::possible_st::b_dot_control:
-                desired_torque = loop_objs::b_dot_conn.compute_torque(curr_att::curr_B_dot);
-                break;
-            case sys_st::possible_st::omega_slew_control:
-                desired_torque = loop_objs::omega_slew_conn.compute_torque(loop_objs::target_ang_vel, curr_att::curr_ang_vel, curr_att::curr_ang_acc, micros());
-                break;
-            case sys_st::possible_st::quat_point_control:
-                desired_torque = loop_objs::quat_point_conn.compute_torque(loop_objs::target_attitude, curr_att::curr_attitude, curr_att::curr_ang_vel);
-                break;
-            default:
-                break;
-            }
-            
-        }
+        task_funcs::control_magnetorquers();
         loop_objs::last_MT_control_check_time_ms = curr_time_ms;
+    }
+
+    if ((curr_time_ms - loop_objs::last_comm_write_check_time_ms) > static_cast<uint32_t>(1e3 / sys_params::COMMUNICATION_WRITE_FREQ_HZ))
+    {
+        task_funcs::write_comm(curr_time_ms);
+        loop_objs::last_comm_write_check_time_ms = curr_time_ms;
+    }
+
+    if ((curr_time_ms - loop_objs::last_LED_check_time_ms) > static_cast<uint32_t>(1e3 / sys_params::DEBUG_LED_FREQ_HZ))
+    {
+        task_funcs::update_LEDs();
+        loop_objs::last_LED_check_time_ms = curr_time_ms;
     }
 }
 
-imu::Vector<3> util_funcs::vector_deriv(const imu::Vector<3> &curr, const imu::Vector<3> &prev, const uint32_t dt_ms)
+void task_funcs::check_watchdogs() noexcept {}
+void task_funcs::update_IMU_data(const uint32_t dt_ms) noexcept
+{
+    for (auto &act : io_objs::magnetorquers)
+    {
+        act.set_lock(true);
+    }
+
+    curr_att::curr_attitude = io_objs::BNO055_IMU.getQuat();
+
+    const auto new_ang_vel = io_objs::BNO055_IMU.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+    curr_att::curr_ang_acc = util_funcs::vector_deriv(new_ang_vel, curr_att::curr_ang_vel, dt_ms);
+    curr_att::curr_ang_vel = std::move(new_ang_vel);
+
+    const auto new_B = io_objs::BNO055_IMU.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
+    curr_att::curr_B_dot = util_funcs::vector_deriv(new_B, curr_att::curr_B, dt_ms);
+    curr_att::curr_B = std::move(new_B);
+
+    for (auto &act : io_objs::magnetorquers)
+    {
+        act.set_lock(false);
+    }
+}
+void task_funcs::control_magnetorquers() noexcept
+{
+    const auto curr_st = loop_objs::sys_controller.get_state();
+    if (curr_st == loop_objs::sys_controller.get_state())
+    {
+        for (auto &actuator : io_objs::magnetorquers)
+        {
+            actuator.drive(0);
+        }
+    }
+    else
+    {
+        imu::Vector<3> desired_torque(0, 0, 0);
+        switch (loop_objs::sys_controller.get_state())
+        {
+        case sys_st::possible_st::direct_B_control:
+            break;
+        case sys_st::possible_st::b_dot_control:
+            desired_torque = loop_objs::b_dot_conn.compute_torque(curr_att::curr_B_dot);
+            break;
+        case sys_st::possible_st::omega_slew_control:
+            desired_torque = loop_objs::omega_slew_conn.compute_torque(loop_objs::system_target.get_vector_data(), curr_att::curr_ang_vel, curr_att::curr_ang_acc, micros());
+            break;
+        case sys_st::possible_st::quat_point_control:
+            desired_torque = loop_objs::quat_point_conn.compute_torque(loop_objs::system_target.get_quat_data(), curr_att::curr_attitude, curr_att::curr_ang_vel);
+            break;
+        default:
+            break;
+        }
+        const auto possible_torque = gnc_math::ActuatorHandler::possible_torque(desired_torque, curr_att::curr_B);
+
+        for (uint8_t i = 0; i < io_objs::magnetorquers.size(); i++)
+        {
+            io_objs::magnetorquers[i].drive(possible_torque[i]);
+        }
+    }
+}
+void task_funcs::write_comm(const uint32_t current_time_ms) noexcept
+{
+    if (io_objs::comm_handler.should_update_telem())
+    {
+        io_objs::comm_handler.update_telem(current_time_ms, curr_att::curr_attitude, curr_att::curr_ang_vel, curr_att::curr_ang_acc,
+                                           curr_att::curr_B, curr_att::curr_B_dot, io_objs::temp_sensors, io_objs::current_sensors);
+    }
+
+    if (io_objs::comm_handler.new_target_update())
+    {
+        loop_objs::system_target = io_objs::comm_handler.get_target_update();
+        loop_objs::sys_controller.transition_st(loop_objs::system_target._target_type);
+    }
+}
+void task_funcs::update_LEDs() noexcept {}
+
+imu::Vector<3> util_funcs::vector_deriv(const imu::Vector<3> &curr, const imu::Vector<3> &prev, const uint32_t dt_ms) noexcept
 {
     return (curr - prev).scale(1e3 / static_cast<float>(dt_ms));
 }
