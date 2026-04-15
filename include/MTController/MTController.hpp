c6ae909a41514256ba98b2dd6b62849bd515357f
darudecookie <darudecookie@gmail.com>
2026-04-15 14:28:49 -0700

    added lock feature

--- include/MTController/MTController.hpp
+++ include/MTController/MTController.hpp
@@ -15,6 +15,9 @@ namespace mt_conn
 
         const sys_st::SysStateMachine &_st_machine;
 
+        bool _locked = false;
+        double _current_dc = 0;
+
     public:
         MTController(const uint8_t in1,
                      const uint8_t in2,
@@ -22,7 +25,10 @@ namespace mt_conn
                      const sys_st::SysStateMachine &st_machine,
                      const bool negative_dc = false) noexcept;
 
-        void drive(const float dutycycle) const noexcept;
+        void drive(const float dutycycle) noexcept;
+
+        void set_lock(const bool is_locked) noexcept;
+        bool is_locked() const noexcept { return this->_locked; }
     };
 
 } // namespace mt_conn
--- src/MTController.cpp
+++ src/MTController.cpp
@@ -22,8 +22,10 @@ namespace mt_conn
         this->drive(0);
     }
 
-    void MTController::drive(float dutycycle) const noexcept
+    void MTController::drive(const float dutycycle) noexcept
     {
+        this->_current_dc = dutycycle;
+
         if ((this->_st_machine.get_state() == sys_st::possible_st::deactivated) || dutycycle == 0)
         {
             digitalWrite(_in1, 1); // both pins high -> brake mode
@@ -33,18 +35,18 @@ namespace mt_conn
 
         if (this->_negative_dc)
         {
-            dutycycle *= -1.0; // flip dc based on wiring 
+            this->_current_dc *= -1.0; // flip dc based on wiring
         }
 
         if (std::abs(dutycycle) > this->_max_dutycycle)
         {
-            // clamp dc at signed max val 
-            dutycycle = (std::signbit(dutycycle) ? -1 : 1) * this->_max_dutycycle;
+            // clamp dc at signed max val
+            this->_current_dc = (std::signbit(dutycycle) ? -1 : 1) * this->_max_dutycycle;
         }
 
         if (dutycycle > 0)
         {
-            
+
             analogWrite(_in1, static_cast<uint8_t>(static_cast<float>(255) * dutycycle));
             analogWrite(_in2, 0);
         }
@@ -53,6 +55,19 @@ namespace mt_conn
 
             analogWrite(_in1, 0);
             analogWrite(_in2, static_cast<uint8_t>(static_cast<float>(255) * -dutycycle));
+        }
+    }
+
+    void MTController::set_lock(const bool is_locked) noexcept
+    {
+        this->_locked = is_locked;
+        if (this->_locked)
+        {
+            this->drive(0);
+        }
+        else
+        {
+            this->drive(this->_current_dc);
         }
     }
 
